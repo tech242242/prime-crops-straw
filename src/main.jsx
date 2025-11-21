0ab197da4ec87f34d1edcf006f882c4144da89fa
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-21 12:24:51 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 
+// Simple error boundary for deployment
+console.log('Prime Crop Straw - Application Starting...')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
