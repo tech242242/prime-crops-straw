356ac9b4b92184a79636abb3b33d39d43fd22a9f
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-21 11:41:47 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import About from './components/About'
+import Products from './components/Products'
+import WhyChoose from './components/WhyChoose'
+import Industries from './components/Industries'
+import Process from './components/Process'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <About />
+      <Products />
+      <WhyChoose />
+      <Industries />
+      <Process />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
