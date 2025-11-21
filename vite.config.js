9e081723a939c8ce89c28f382ae0e447d6afcdac
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-21 13:01:43 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
+  base: './',
+  server: {
+    port: 3000,
+    host: true
   }
 })
